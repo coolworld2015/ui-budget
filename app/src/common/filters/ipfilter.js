79a8136f6b83bba140c6fcaef295d33acec094d2
coolworld2015 <wintermute2015@ukr.net>
2016-08-02 20:56:12 +0300

    [app] - updated heroku

--- app/src/common/filters/ipfilter.js
+++ app/src/common/filters/ipfilter.js
@@ -1,14 +1,14 @@
 (function () {
     "use strict";
 
-        angular
-            .module('app')
-            .filter('ipfilter', ipfilter);              // ::ffff:10.136.89.162
+	angular
+		.module('app')
+		.filter('ipfilter', ipfilter);              // ::ffff:10.136.89.162
 
-        function ipfilter() {
-            return function (item) {
-                var b = item.split(':');
-                return b[3];
-            };
-        }
+	function ipfilter() {
+		return function (item) {
+			var b = item.split(':');
+			return b[3];
+		};
+	}
 })();
